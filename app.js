e739ee5db7ef3aa0347d26e679ba93e2b4a33c5a
hieu <hieuunguyenn63@gmail.com>
2025-05-10 18:07:39 +0300

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017");
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.on('open', () => console.log('Connected to Databese'))
+
+app.use(express.json())
+const PORT = 3000 || 7000
+app.listen(PORT, () => console.log( `[SERVER] Started on port ${PORT}`))
